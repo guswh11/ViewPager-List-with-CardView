0e9484247eaede4fcd2a8a5c318198256c3d7af9
Hyunjin <guswh11@naver.com>
2019-09-16 16:45:42 +0900

    Init

---
+++ app/src/main/java/com/example/viewpager_list_with_cardview/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.viewpager_list_with_cardview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
